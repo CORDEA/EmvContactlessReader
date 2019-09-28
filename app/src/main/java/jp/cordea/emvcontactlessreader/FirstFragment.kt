134760815c4e244c9e09deeeaed6259021d30fe9
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-28 15:05:44 +0900

    add view model

--- app/src/main/java/jp/cordea/emvcontactlessreader/FirstFragment.kt
+++ app/src/main/java/jp/cordea/emvcontactlessreader/FirstFragment.kt
@@ -5,8 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 
 class FirstFragment : Fragment() {
+    private val activityViewModel: MainViewModel by activityViewModels()
+    private val viewModel: FirstViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,5 +21,9 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        activityViewModel.tag
+            .observe(viewLifecycleOwner, Observer {
+                viewModel.handleTag(it)
+            })
     }
 }
---
+++ app/src/main/java/jp/cordea/emvcontactlessreader/FirstViewModel.kt
@@ -0,0 +1,9 @@
+package jp.cordea.emvcontactlessreader
+
+import android.nfc.Tag
+import androidx.lifecycle.ViewModel
+
+class FirstViewModel : ViewModel() {
+    fun handleTag(tag: Tag) {
+    }
+}
