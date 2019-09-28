41d908cc56a43a57646c5cb136c59ecde597aaf5
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-28 14:32:34 +0900

    read nfc tags

--- app/src/main/java/jp/cordea/emvcontactlessreader/MainActivity.kt
+++ app/src/main/java/jp/cordea/emvcontactlessreader/MainActivity.kt
@@ -1,5 +1,10 @@
 package jp.cordea.emvcontactlessreader
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NfcAdapter
+import android.nfc.tech.IsoDep
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -7,10 +12,39 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val intent by lazy {
+        PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this, this::class.java).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+            0
+        )
+    }
+
+    private val filters = arrayOf(IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED))
+    private val techList = arrayOf(arrayOf(IsoDep::class.java.name))
+
+    private lateinit var adapter: NfcAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        adapter = NfcAdapter.getDefaultAdapter(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.enableForegroundDispatch(this, intent, filters, techList)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        adapter.disableForegroundDispatch(this)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
