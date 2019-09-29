7fff079706a71c9ea6fef584f1878fe2d91b01cb
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-29 18:57:36 +0900

    get response

--- app/src/main/java/jp/cordea/emvcontactlessreader/FirstViewModel.kt
+++ app/src/main/java/jp/cordea/emvcontactlessreader/FirstViewModel.kt
@@ -1,9 +1,21 @@
 package jp.cordea.emvcontactlessreader
 
 import android.nfc.Tag
+import android.nfc.tech.IsoDep
 import androidx.lifecycle.ViewModel
 
 class FirstViewModel : ViewModel() {
     fun handleTag(tag: Tag) {
+        IsoDep.get(tag).use {
+            it.connect()
+            val command = SelectCommand(it)
+            val adf = command.select(ApplicationIdentifier.VISA)
+            val status = Status.from(adf)
+            if (status is Failure) {
+                return
+            }
+            val tags = Pdol.extractTags(adf)
+            val response = GpoCommand(it).gpo(tags.map { it.fill() }.flatten())
+        }
     }
 }
