d072196ea2806051a6a67d1fc72546ca299cb10f
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-29 18:56:41 +0900

    return tags

--- app/src/main/java/jp/cordea/emvcontactlessreader/Pdol.kt
+++ app/src/main/java/jp/cordea/emvcontactlessreader/Pdol.kt
@@ -1,16 +1,14 @@
 package jp.cordea.emvcontactlessreader
 
-class Pdol(
-) {
-    companion object {
-        private val START = listOf(0x9F, 0x38).map { it.toByte() }
+object Pdol {
+    private val START = listOf(0x9F, 0x38).map { it.toByte() }
 
-        fun from(data: ByteArray) {
-            val index = data.indices
-                .indexOfFirst { data[it] == START[0] && data[it + 1] == START[1] }
-            val length = data[index + 2].toInt()
-            val startIndex = index + 3
-            val rawPdol = data.slice(startIndex until startIndex + length)
-        }
+    fun extractTags(data: ByteArray): List<PdolTag> {
+        val index = data.indices
+            .indexOfFirst { data[it] == START[0] && data[it + 1] == START[1] }
+        val length = data[index + 2].toInt()
+        val startIndex = index + 3
+        val rawPdol = data.slice(startIndex until startIndex + length)
+        return PdolTag.from(rawPdol)
     }
 }
