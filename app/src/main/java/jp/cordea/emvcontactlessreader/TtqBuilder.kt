7f57d1a36b3eeeb81c6e4bd8e5147f3e9aab8fde
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-29 12:55:46 +0900

    add TTQ builder

---
+++ app/src/main/java/jp/cordea/emvcontactlessreader/TtqBuilder.kt
@@ -0,0 +1,51 @@
+package jp.cordea.emvcontactlessreader
+
+class TtqBuilder {
+    private val ttq = arrayOf(0x00, 0x00, 0x00, 0x00)
+
+    fun supportContactless() = apply {
+        ttq[0] = ttq[0] or 0x40
+    }
+
+    fun supportEmvMode() = apply {
+        ttq[0] = ttq[0] or 0x20
+    }
+
+    fun supportContact() = apply {
+        ttq[0] = ttq[0] or 0x10
+    }
+
+    fun offlineOnly() = apply {
+        ttq[0] = ttq[0] or 0x08
+    }
+
+    fun supportOnlinePin() = apply {
+        ttq[0] = ttq[0] or 0x04
+    }
+
+    fun supportSignature() = apply {
+        ttq[0] = ttq[0] or 0x02
+    }
+
+    fun supportOfflineDataAuthentication() = apply {
+        ttq[0] = ttq[0] or 0x01
+    }
+
+    fun requestOnlineCryptogram() = apply {
+        ttq[1] = ttq[1] or 0x80
+    }
+
+    fun requestCvm() = apply {
+        ttq[1] = ttq[1] or 0x40
+    }
+
+    fun supportConsumerDevice() = apply {
+        ttq[2] = ttq[2] or 0x40
+    }
+
+    fun supportFdda() = apply {
+        ttq[3] = ttq[3] or 0x80
+    }
+
+    fun build(): Array<Int> = ttq
+}
